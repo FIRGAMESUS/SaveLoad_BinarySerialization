55813ef968936747cdc31ea72077193b8e63a368
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-31 17:09:04 +0300

    SaveLoad_BinarySerialization (28.09.19)

---
+++ Assets/SaveLoadManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SaveLoadManager : MonoBehaviour
+{
+    string filePath;
+    TestSript GM;
+
+    private void Start()
+    {
+        filePath = Path.Combine(Application.persistentDataPath, "save.gamesave");
+        GM = GetComponent<TestSript>();
+    }
+
+    public void SaveGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = new FileStream(filePath, FileMode.Create);
+
+        Save save = new Save();
+
+        save.Score = GM.number;
+
+        bf.Serialize(fs, save);
+        fs.Close();
+
+    }
+
+    public void LoadGame()
+    {
+        if (!File.Exists(filePath)) return;
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = new FileStream(filePath, FileMode.Open);
+
+        Save save = (Save)bf.Deserialize(fs);
+        fs.Close();
+
+        GM.number = save.Score;
+        GM.text.text = GM.number.ToString();
+    }
+}
+
+[System.Serializable]
+public class Save
+{
+    public int Score;
+}
---
+++ Assets/TestSript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestSript : MonoBehaviour
+{
+    public int number = 0;
+    public Text text;
+
+    private void Start()
+    {
+        text.text = number.ToString();
+    }
+
+    /*
+    private void FixedUpdate()
+    {
+        number++;
+        text.text = number.ToString();
+    }
+    */
+    public void Pushing()
+    {
+        number++;
+        text.text = number.ToString();
+    }
+    
+}
